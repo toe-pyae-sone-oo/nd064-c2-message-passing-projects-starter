f1833fdf65035bcf0f4af6e5e06540fdf751adaa
Toe Pyae Sone Oo <toepyae.sone@shopee.com>
2023-01-08 19:40:53 +0800

    set up logger in consumer

--- modules/consumer/main.py
+++ modules/consumer/main.py
@@ -3,22 +3,26 @@ import sys
 import logging
 from kafka import KafkaConsumer
 
+def setup_logger():
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("udaconnect-kafka-consumer")
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("udaconnect-kafka-consumer")
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stdout_handler)
 
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.DEBUG)
-logger.addHandler(stdout_handler)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    logger.addHandler(stderr_handler)
 
-stderr_handler = logging.StreamHandler(sys.stderr)
-stderr_handler.setLevel(logging.ERROR)
-logger.addHandler(stderr_handler)
+    logger.propagate = False
 
-logger.propagate = False
+    return logger
 
 TOPIC_NAME = os.environ["KAFKA_TOPIC"]
 KAFKA_SERVER = os.environ["KAFKA_SERVER"]
+
+logger = setup_logger()
 
 consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_SERVER)
 for message in consumer:
