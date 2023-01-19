d608e2ee17cb161813a2e7e2d62c80cc4ba29fb2
Toe Pyae Sone Oo <toepyae.sone@shopee.com>
2023-01-20 00:45:47 +0800

    cleanup persons

--- modules/persons/app/udaconnect/schemas.py
+++ modules/persons/app/udaconnect/schemas.py
@@ -1,18 +1,7 @@
-from app.udaconnect.models import Connection, Location, Person
+from app.udaconnect.models import Person
 from geoalchemy2.types import Geometry as GeometryType
 from marshmallow import Schema, fields
 from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
-
-
-class LocationSchema(Schema):
-    id = fields.Integer()
-    person_id = fields.Integer()
-    longitude = fields.String(attribute="longitude")
-    latitude = fields.String(attribute="latitude")
-    creation_time = fields.DateTime()
-
-    class Meta:
-        model = Location
 
 
 class PersonSchema(Schema):
@@ -24,7 +13,3 @@ class PersonSchema(Schema):
     class Meta:
         model = Person
 
-
-class ConnectionSchema(Schema):
-    location = fields.Nested(LocationSchema)
-    person = fields.Nested(PersonSchema)
